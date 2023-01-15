05abb2df6e21ebeba196a0dd54feabf13636b4d9
manojap <manojap@outlook.com>
2023-01-15 22:35:04 +0530

    .

--- app/routes/$.jsx
+++ app/routes/$.jsx
@@ -1,19 +1,22 @@
 import React from "react";
- 
+import styles from "../styles/NF.css";
+export const links = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+};
 export default function NF() {
   return (
-    <div className="flex justify-center">
-      <div className="notfound ">
-        <div className="notfound-404 ">
-          <h3>Oops! Page not found</h3>
-          <h1>
-            <span>4</span>
-            <span>0</span>
-            <span>4</span>
-          </h1>
-        </div>
-        <h2>we are sorry, but the page you requested was not found</h2>
-      </div>
+    <div id='oopss'>
+    <div id='error-text'>
+        <span>404</span>
+        <p>PAGE NOT FOUND</p>
+        <p class='hmpg'><a href='/' class="back">Back To Home</a></p>
     </div>
+
+</div>
   );
 }
